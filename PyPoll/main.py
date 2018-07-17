3b9b898c8499f73ac3536d824e54ec5598cd7b38
Travis Genovese <37942161+pickle-weasel@users.noreply.github.com>
2018-07-16 18:01:45 -0700

    Add files via upload

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,41 +8,41 @@ with open(csvpath, 'r') as csvfile:
 
     csvheader = next(reader)
 
-    voter_id = []
-    county = []
     candidate = []
-    correy = []
-    o_tooley = []
-    li = []
-    khan = []
 
     for row in reader:
-        voter_id.append(row[0])
-        county.append(row[1])
         candidate.append(row[2])
-        
-        
-        if row[2] == 'Correy':
-            correy.append(row[2])
-        elif row[2] == "O'Tooley":
-            o_tooley.append(row[2])
-        elif row[2] == 'Li':
-            li.append(row[2])
-        else:
-            khan.append(row[2])
+
+    candidate_count = [[x,candidate.count(x)] for x in set(candidate)]
+    
+    votes = []
+    name = []
+
+    for row in candidate_count:
+        name.append(row[0])
+        votes.append(row[1])
+
+    candidate_zip = zip(name, votes)
+    candidate_list = list(candidate_zip)
+   
+    winner = max(votes)
+
+    for row in candidate_list:
+        if row[1] == winner:
+            winner_name = row[0]       
             
 total_votes = len(candidate)
 
-correy_total = len(correy)
+correy_total = candidate.count('Correy')
 correy_percent = int(correy_total) / int(total_votes)
 
-o_tooley_total = len(o_tooley)
+o_tooley_total = candidate.count("O'Tooley")
 o_tooley_percent = o_tooley_total / total_votes
 
-li_total = len(li)
+li_total = candidate.count('Li')
 li_percent = li_total / total_votes
 
-khan_total = len(khan)
+khan_total = candidate.count('Khan')
 khan_percent = khan_total / total_votes
 
 print(f'Election Results')
@@ -54,5 +54,5 @@ print(f'Correy: {correy_percent:.3%} ({correy_total})')
 print(f'Li: {li_percent:.3%} ({li_total})')
 print(f"O'Tooley: {o_tooley_percent:.3%} ({o_tooley_total})")
 print(f'-------------------------')
-print(f'Winner: ')
+print(f'Winner: {winner_name}')
 print(f'-------------------------')
